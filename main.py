49387324d364ccbba2305832b01efffc2513e0c0
Vivaan <87912804+VivaanCode@users.noreply.github.com>
2026-02-09 20:54:42 -0800

    i think they call it a boilerplate or something

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import psycopg2
 db_url = os.getenv("DATABASE_URL")
 app = Flask('app', static_folder="static", template_folder="templates")
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
 @app.route("/")
 def index():
     return render_template("index.html")
+
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
